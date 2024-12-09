0b8414f181af5d523c4ccf2b8b89c6799863ad60
Arnon Erez <arnonerez0@gmail.com>
2024-12-09 17:47:37 +0000

    update to current state. semi working...

--- frontend/src/providers/authelia/oidc_config.ts
+++ frontend/src/providers/authelia/oidc_config.ts
@@ -1,14 +1,28 @@
 import { WebStorageStateStore } from "oidc-client-ts";
 
+// const app_url = import.meta.env.VITE_APP_URL || "https://smplwise.com";
+// const auth_url = import.meta.env.VITE_AUTH_URL || "https://smplwise.com/auth";
+// const auth_client_id = import.meta.env.VITE_AUTH_CLIENT_ID || "frontend";
+// const auth_client_secret = import.meta.env.VITE_AUTH_CLIENT_SECRET || "QN_EVekFgTEQPhuLNNnY9JbnnIjxP4nha9BiypIu6M9qrHVw6jfLvd~rPCcnneDzdP2QZoC3";
+// const auth_redirect_uri = import.meta.env.VITE_AUTH_REDIRECT_URI || "https://smplwise.com/callback";
+
+const currentHost = window.location.origin;
+
+const app_url = currentHost;
+const auth_url = `${currentHost}/auth`;
+const auth_client_id = "frontend";
+const auth_client_secret = "QN_EVekFgTEQPhuLNNnY9JbnnIjxP4nha9BiypIu6M9qrHVw6jfLvd~rPCcnneDzdP2QZoC3";
+const auth_redirect_uri = `${currentHost}/callback`;
+
 export const oidcConfig = {
-    authority: import.meta.env.VITE_AUTH_URL,
-    client_id: import.meta.env.VITE_AUTH_CLIENT_ID,
-    client_secret: import.meta.env.VITE_AUTH_CLIENT_SECRET,
-    redirect_uri: `${import.meta.env.VITE_APP_URL}/callback`,
+    authority: auth_url,
+    client_id: auth_client_id,
+    client_secret: auth_client_secret,
+    redirect_uri: `${auth_redirect_uri}`,
     scope: "openid profile email groups",
     // client_authentication: "client_secret_post",
     loadUserInfo: true,
-    post_logout_redirect_uri: `${import.meta.env.VITE_APP_URL}`,
+    post_logout_redirect_uri: `${app_url}`,
     revoke_tokens_on_signout: true,
     omit_scopes_when_requesting: true,
     // automaticSilentRenew: true,
